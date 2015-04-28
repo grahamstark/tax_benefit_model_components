5301082ac6fa6268892094de71f96787128bbb8c
Graham Stark <graham.stark@virtual-worlds.biz>
2015-04-28 10:06:43 +0100

    Cut down print of Boolean Arrays in T_Utils

--- src/general/t_utils.adb
+++ src/general/t_utils.adb
@@ -237,7 +237,7 @@ package body T_Utils is
    begin
       for i in b'Range loop
          if( b(i) ) then
-            outs := outs & T'Image( i ) & SEPARATOR & Boolean'Image(b(i));
+            outs := outs & T'Image( i );
             if( break_lines ) then
                outs := outs & LINE_BREAK;
             else
@@ -245,7 +245,7 @@ package body T_Utils is
             end if;
          end if;
       end loop;
-      if( not break_lines ) then
+      if( not break_lines ) and ( Length( outs ) > 0 ) then
          Delete_Last_Character( outs );
       end if;
       return To_String(outs);
