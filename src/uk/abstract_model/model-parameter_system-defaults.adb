da91b797595bfb273e4a263189d002a83e092e7c
Graham Stark <graham.stark@virtual-worlds.biz>
2015-06-03 20:11:53 +0100

    halfway commit on default values

--- src/uk/abstract_model/model-parameter_system-defaults.adb
+++ src/uk/abstract_model/model-parameter_system-defaults.adb
@@ -1,6 +1,5 @@
 with Ada.Assertions;
 with Ada.Text_IO;
-with Example_Data;
 
 package body Model.Parameter_System.Defaults is
 
@@ -119,6 +118,22 @@ package body Model.Parameter_System.Defaults is
                   employer_in_rates,
                   employer_out_rates );
             end;
+        when 2015   => -- WEEKLY!!!!
+            declare
+               employee_in_rates  : V4 := ( 0.0, 0.0, 12.0, 2.0 );
+               employee_out_rates : V4 := ( 0.0, 0.0, 10.6, 2.0 ); -- 2 1.6-rebate
+               employer_in_rates  : V4 := ( 0.0, 0.0, 13.80, 13.8 );
+               employer_out_rates : V4 := ( 0.0, 0.0, 10.4, 10.4 );
+               bands              : V4 := ( 111.0, 153.0, 805.00, Amount'Last );
+            begin
+               Set_Rates_And_Bands_NI(
+                  sys,
+                  bands,
+                  employee_in_rates,
+                  employee_out_rates,
+                  employer_in_rates,
+                  employer_out_rates );
+            end;
         when others => null;
       end case;
       return sys;
@@ -200,6 +215,16 @@ package body Model.Parameter_System.Defaults is
                sys.personal_allowance := 9_440.00;
                Set_Rates_And_Bands( sys, bands, non_savings_income, savings_income, dividends_income );
             end;
+        when 2015   =>
+            declare
+               non_savings_income : V4 := ( 20.0, 20.0, 40.0, 45.0 );
+               savings_income     : V4 := ( 10.0, 20.0, 40.0, 45.0 );
+               dividends_income   : V4 := ( 10.0, 10.0, 32.50, 32.50 );
+               bands              : V4 := ( 4_905.0, 31_785.0, 150_000.0, Amount'Last );
+            begin
+               sys.personal_allowance := 10_600.00;
+               Set_Rates_And_Bands( sys, bands, non_savings_income, savings_income, dividends_income );
+            end;
         when others => null;
       end case;
       return sys;
@@ -230,41 +255,80 @@ package body Model.Parameter_System.Defaults is
       -- FIXME  CAPITAL/all taxable benefits/Income Tax Split/
       case year is
          when 2013 =>
-            uc.allowances.single_claimant_aged_under_25 :=  246.81 * 12.0;
-            uc.allowances.single_claimant_aged_25_or_over :=  311.55 * 12.0;
-            uc.allowances.joint_claimants_both_aged_under_25 :=  387.42 * 12.0;
-            uc.allowances.joint_claimants_either_aged_25_or_over :=  489.06 * 12.0;
-            uc.allowances.first_child_or_qualifying_young_person :=  272.08 * 12.0;
-            uc.allowances.second_and_each_subsequent_child_or_qualifying_young_person :=  226.67*12.0;
+            uc.allowances.single_claimant_aged_under_25 :=  247.04;
+            uc.allowances.single_claimant_aged_25_or_over :=  311.55;
+            uc.allowances.joint_claimants_both_aged_under_25 :=  387.42;
+            uc.allowances.joint_claimants_either_aged_25_or_over :=  489.06;
+            uc.allowances.first_child_or_qualifying_young_person :=  272.08;
+            uc.allowances.second_and_each_subsequent_child_or_qualifying_young_person :=  226.67;
 
-            uc.disregards.single_with_housing_no_children :=  111.0*12.0;
-            uc.disregards.single_with_housing_with_children :=  263.0*12.0;
-            uc.disregards.single_with_housing_limited_work_capacity :=  192.0*12.0;
+            uc.disregards.single_with_housing_no_children :=  111.0;
+            uc.disregards.single_with_housing_with_children :=  263.0;
+            uc.disregards.single_with_housing_limited_work_capacity :=  192.0;
 
-            uc.disregards.single_no_housing_no_children :=  111.0*12.0;
-            uc.disregards.single_no_housing_with_children :=  734.0*12.0;
-            uc.disregards.single_no_housing_limited_work_capacity :=  647.0*12.0;
+            uc.disregards.single_no_housing_no_children :=  111.0;
+            uc.disregards.single_no_housing_with_children :=  734.0;
+            uc.disregards.single_no_housing_limited_work_capacity :=  647.0;
 
-            uc.disregards.couple_with_housing_no_children :=  111.0*12.0;
-            uc.disregards.couple_with_housing_with_children :=  222.0*12.0;
-            uc.disregards.couple_with_housing_limited_work_capacity :=  192.0*12.0;
+            uc.disregards.couple_with_housing_no_children :=  111.0;
+            uc.disregards.couple_with_housing_with_children :=  222.0;
+            uc.disregards.couple_with_housing_limited_work_capacity :=  192.0;
 
-            uc.disregards.couple_no_housing_no_children :=  111.0*12.0;
-            uc.disregards.couple_no_housing_with_children :=  536.0*12.0;
-            uc.disregards.couple_no_housing_limited_work_capacity :=  647.0*12.0;
+            uc.disregards.couple_no_housing_no_children :=  111.0;
+            uc.disregards.couple_no_housing_with_children :=  536.0;
+            uc.disregards.couple_no_housing_limited_work_capacity :=  647.0;
 
-            uc.one_bedroom_in_shared_accommodation_rate := 12.0 * 234.37; 
+            uc.one_bedroom_in_shared_accommodation_rate := 234.37; 
             -- straight unweighted average over all wales councils; 
             -- see http://wales.gov.uk/topics/housingandcommunity/housing/private/renting/rentofficers/publications/lha13/?lang=en
             uc.withdrawal_rate :=  65.0/100.0;
-            uc.limited_capability_for_work :=  123.62 * 12.0;
-            uc.limited_capability_for_work_and_work_related_activity :=  303.66 * 12.0;
-            uc.caring_for_a_severely_disabled_person_for_at_least_35_hours_a_week :=  144.70 * 12.0;
+            uc.limited_capability_for_work :=  123.62
+            uc.limited_capability_for_work_and_work_related_activity :=  303.66
+            uc.caring_for_a_severely_disabled_person_for_at_least_35_hours_a_week :=  144.70
             uc.childcare_costs_percent :=  70.0/100.0;
-            uc.maximum_childcare_award_families :=  500.0*52.0;
-            uc.maximum_childcare_award_lone_parents :=  350.0*52.0;
-            uc.maximum_payment_families :=   500.0*52.0;
-            uc.maximum_payment_singles  :=   350.0*52.0;
+            uc.maximum_childcare_award_families :=  500.0;
+            uc.maximum_childcare_award_lone_parents :=  350.0;
+            uc.maximum_payment_families :=   500.0;
+            uc.maximum_payment_singles  :=   350.0;
+         when 2015 =>
+            uc.allowances.single_claimant_aged_under_25 :=  251.77;
+            uc.allowances.single_claimant_aged_25_or_over :=  317.82;
+            uc.allowances.joint_claimants_both_aged_under_25 :=  395.20;
+            uc.allowances.joint_claimants_either_aged_25_or_over :=  498.89;
+            uc.allowances.first_child_or_qualifying_young_person :=  277.08;
+            uc.allowances.second_and_each_subsequent_child_or_qualifying_young_person :=  231.67;
+
+            uc.disregards.single_with_housing_no_children :=  111.0;
+            uc.disregards.single_with_housing_with_children :=  263.0;
+            uc.disregards.single_with_housing_limited_work_capacity :=  192.0;
+
+            uc.disregards.single_no_housing_no_children :=  111.0;
+            uc.disregards.single_no_housing_with_children :=  734.0;
+            uc.disregards.single_no_housing_limited_work_capacity :=  647.0;
+
+            uc.disregards.couple_with_housing_no_children :=  111.0;
+            uc.disregards.couple_with_housing_with_children :=  222.0;
+            uc.disregards.couple_with_housing_limited_work_capacity :=  192.0;
+
+            uc.disregards.couple_no_housing_no_children :=  111.0;
+            uc.disregards.couple_no_housing_with_children :=  536.0;
+            uc.disregards.couple_no_housing_limited_work_capacity :=  647.0;
+
+            uc.one_bedroom_in_shared_accommodation_rate := 234.37;
+            -- FIXME!!!
+            -- straight unweighted average over all wales councils; 
+            -- see http://wales.gov.uk/topics/housingandcommunity/housing/private/renting/rentofficers/publications/lha13/?lang=en
+            uc.withdrawal_rate :=  65.0/100.0;
+            -- DON
+            uc.limited_capability_for_work :=  126.11;
+            uc.limited_capability_for_work_and_work_related_activity :=  315.60;
+            uc.caring_for_a_severely_disabled_person_for_at_least_35_hours_a_week :=  150.30;
+            uc.childcare_costs_percent :=  70.0/100.0;
+            uc.maximum_childcare_award_families :=  2_000.0;
+            -- FIXME what are these numbers???
+            uc.maximum_childcare_award_lone_parents :=  350.0/12.0;
+            uc.maximum_payment_families :=   2_000.0;
+            uc.maximum_payment_singles  :=   350.0/12.0;
          when others => null;
       end case;
       return uc;
@@ -275,105 +339,114 @@ package body Model.Parameter_System.Defaults is
    begin
       case year is
          when 2013 =>
-           sys.first_child := 20.30*52.0;
-           sys.additional_children := 13.40*52.0;
-           sys.guardians_allowance := 15.90*52.0;
+           sys.first_child := 20.30;
+           sys.additional_children := 13.40;
+           sys.guardians_allowance := 15.90;
+         when 2015 =>
+           sys.first_child := 20.50;
+           sys.additional_children := 13.55;
+           sys.guardians_allowance := 16.35;
          when others => null;
       end case;
       return sys;
    end Get_Child_Benefit_System;
 
-   function Get_Student_Loan_And_Grant_System(
-      year                  : Year_Number;
-      plan_type             : Loan_Plan_Type;
-      num_maintenance_bands : Positive;
-      num_algfe_bands       : Positive ) return Student_Loan_And_Grant_System is
-      sys : Student_Loan_And_Grant_System( plan_type, num_maintenance_bands, num_algfe_bands );
-   begin
-      case year is
-         when 2013 =>
-            sys.income_limit:= 21_000.00;
-            sys.payment_rate := 0.09;
-            Put_Line( "sys.grant_income_limits'Length " & sys.grant_income_limits'Length'Img );
-            sys.grant_income_limits := ( 1=>18_370.0, 2=>25_000.0, 3=>30_000.0, 4=>34_000.0, 5=>40_000.0, 6=>45_000.0, 7=>50_020.0 );
-            sys.assembly_learning_grant := ( 5_161.0, 3_347.0, 2_099.0, 1_142.0, 734.0, 393.0, 50.0 );
-            sys.maintenance_loan_away := ( 2_575.0, 3_477.0, 4_101.0, 4_579.0, 4_783.0, 4_954.0, 5_125.0 );
-            sys.maintenance_loan_london := ( 4_640.0, 5_542.0, 6_166.0, 6_644.0, 6_848.0, 7_019.0, 7_190.0 );
-            sys.maintenance_loan_home := ( 1_412.0, 2_314.0, 2_938.0, 3_416.0, 3_620.0, 3_791.0, 3_962.0 );
-            sys.tuition_fee_loan := 3_575.0;
-            sys.tuition_fee_grant := 5_425.0;
-            sys.ema := 30.0*52.0;
-            sys.ema_income_limit := 20_817.0;
-            sys.ema_income_limit_w_kids := 23_077.00;
-            sys.algfe := ( 1_500.0, 750.0, 450.0 );
-            sys.algfe_income_limit := ( 6120.0, 12_236.00, 18_370.00 );
-            sys.higher_education_bursary := 2_000.0;
-            -- per week 1 room average in wales * 22 weeks away from course
-            sys.average_accommodation_costs_out_of_term_time := Example_Data.ONE_BEDROOM_FLAT_SOCIAL_HOUSING*22.0;
-         when others => null;
-      end case;
-       return sys;
-      -- plus some stuff about higher interest above 41k
-   end Get_Student_Loan_And_Grant_System;
-
-   function Get_Foster_Payment_System( year : Year_Number ) return Foster_Payment_System is
-      fcs : Foster_Payment_System;
-   begin
-      case year is
-         when 2013 =>
-            
-            -- over 18s from Table 7.1: Payments to staying put providers DFE
-            -- rest from http://www.fostering.net/news/2011/fostering-allowances-launched-wales#.UnGn4XiMw8E
-            fcs.age_limits := ( 4, 15, 18, 21 );
-            fcs.minimum_amounts := ( 149.00*52.0, 136.00*52.0, 170.00*52.00, 200.00*52.00 );
-            fcs.childs_contributions := ( 0.0, 0.0, 0.0, 100.00*52.00 );
-         when others => null; -- note these are actuall published to 2016
-      end case;
-      return fcs;
-   end  Get_Foster_Payment_System;
-
-   function Get_Social_Care_Costs( year : Year_Number ) return Social_Care_Costs is
-      sc : Social_Care_Costs;
-   begin
-      case year is
-         when 2013 => 
-            sc.when_i_am_ready := ( 
-               initial_review       => 816.0,
-               maintenance          => 120.0*52.0,
-               placement_ceases     => 775.0,
-               bi_annual_review     => 446.0 );
-            sc.current_system := (
-               initial_review       => 321.0,
-               maintenance          => 321.0*52.0,
-               placement_ceases     => 373.0,
-               bi_annual_review     => 556.0 );
-         when others => null;
-      end case;
-      return sc;
-   end Get_Social_Care_Costs;
-   
    function Get_Indirect_Taxes( year : Year_Number ) return Indirect_Taxes is
       indir : Indirect_Taxes;
    begin
       case year is
-         when 2013 => 
+         when 2013 .. 2015 => 
             indir.vat := 0.20;
          when others => null;
       end case;
       return indir;
    end  Get_Indirect_Taxes;
    
-   function Get_Complete_System( year : Year_Number ) return Complete_System is
-      sys : Complete_System( plan_type => plan_2, num_maintenance_bands => 7, num_algfe_bands => 3 );
+   function Get_AA( year : Year_Number ) return Attendance_Allowance_System is
+      aa : Attendance_Allowance_System;
    begin
-      sys.cb := Get_Child_Benefit_System( year );
+      case year is
+         when 2015 =>
+            aa.low_age         := 65;
+            aa.high_age        := Age_Range'Last;
+            aa.benefit_rate    := ( high => 81.3, low=> 54.45 );
+            aa.test_generosity : Rate := 1.0;
+            aa.preserve_for_existing_claimants := True;
+         when others => null;
+      end case;
+      return aa;
+   end Get_AA;
+   
+   
+   function Get_DLA( year : Year_Number ) return Disability_Living_Allowance_System is
+      dla : Disability_Living_Allowance_System;
+   begin
+      case year is
+      when 2015 =>
+         dla.mobility.low_age         := 0;
+         dla.mobility.high_age        := 64;
+         dla.mobility.benefit_rate    := ( low => 21.55, high=>56.75 );
+         dla.mobility.test_generosity := 1.0;
+         dla.mobility.preserve_for_existing_claimants := True;
+         dla.care.low_age             := 0;
+         dla.care.high_age            := 64;
+         dla.care.benefit_rate        := ( high=>81.30, middle=>54.45, low=>21.55 );
+         dla.care.test_generosity     := 1.0;
+         dla.care.preserve_for_existing_claimants := True;
+      when others => null;
+      end case;
+      return dla;
+   end Get_DLA;
+   
+    type Pension_System is record
+      age_men   : Age_Range;
+      age_women : Age_Range;
+      citizens_pension  : Boolean := False;
+      class_a : Amount;
+      preserve_for_existing_claimants : Boolean;
+   end record;
+   
+   type Guaranteed_Credit_System is record
+      single : Amount;
+      couple : Amount;
+      carer_single : Amount;
+      severe_disability_single : Amount;
+      severe_disability_couple : Amount;
+      incomes : Included_Incomes_Array := Get_Default_Incomes( guaranteed_pension_credit );
+      earnings_disregard : Amount;
+      benefit_disregard  : Amount; -- need a list of benefits this applies to
+      preserve_for_existing_claimants : Boolean;
+   end record;
+   
+   type Savings_Credit_System is record
+      threshold_single : Amount;
+      threshold_couple : Amount;
+      maximum_single : Amount;
+      maximum_couple : Amount;
+      withdrawal_rate : Rate;
+      incomes : Included_Incomes_Array  := Get_Default_Incomes( savings_credit );
+      qualifying_incomes : Included_Incomes_Array  := Get_Default_Incomes( savings_credit_qualifying_income );
+      earnings_disregard : Amount;
+      benefit_disregard  : Amount;
+      preserve_for_existing_claimants : Boolean;
+   end record;
+  
+   type Pension_Credit_System is record
+      guaranteed_credit : Guaranteed_Credit_System;
+      savings_credit : Savings_Credit_System;
+   end record;
+   
+   
+   function Get_Complete_System( year : Year_Number ) return Complete_System is
+      sys : Complete_System;
+   begin
+      sys.benefits.child_benefit := Get_Child_Benefit_System( year );
+      sys.benefits.universal_credit := Get_Universal_Credit_System( year );
+      
       sys.it := Get_Income_Tax_System( year );
       sys.ni := Get_National_Insurance_System( year );
-      sys.uc := Get_Universal_Credit_System( year );
-      sys.sl := Get_Student_Loan_And_Grant_System( year => year, plan_type => plan_2, num_maintenance_bands => 7, num_algfe_bands => 3 );
-      sys.fp := Get_Foster_Payment_System( year );
-      sys.sc := Get_Social_Care_Costs( year );
       sys.indir := Get_Indirect_Taxes( year );
+      sys.benefits.d         
       return sys;
     end Get_Complete_System;
 
--- src/uk/abstract_model/model-parameter_system-defaults.ads
+++ src/uk/abstract_model/model-parameter_system-defaults.ads
@@ -11,11 +11,6 @@ private
    function Get_Income_Tax_System( year : Year_Number ) return Income_Tax_System;
    function Get_National_Insurance_System( year : Year_Number ) return National_Insurance_System;
    function Get_Universal_Credit_System( year : Year_Number ) return Universal_Credit_System;
-   function Get_Student_Loan_And_Grant_System(
-      year : Year_Number;
-      plan_type             : Loan_Plan_Type;
-      num_maintenance_bands : Positive;
-      num_algfe_bands       : Positive  ) return Student_Loan_And_Grant_System;
    function Get_Child_Benefit_System( year : Year_Number ) return Child_Benefit_System;
 
 end Model.Parameter_System.Defaults;
