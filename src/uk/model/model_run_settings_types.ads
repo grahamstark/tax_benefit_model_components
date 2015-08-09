984a27a5a41c9ddae88cf9df2fa6bb564cbfb76e
Graham Stark <graham.stark@virtual-worlds.biz>
2015-08-09 19:20:46 +0100

    rearrangement of run settings to cut down dependencies; may break some old models

---
+++ src/uk/model/model_run_settings_types.ads
@@ -0,0 +1,19 @@
+package Model_Run_Settings_Types is
+
+   type Target_Type is ( off_diagonal_index, costs_index, targetting_index );
+  
+   type Phase_Type is ( 
+      not_started, 
+      queued, 
+      run_starting, 
+      pre_calculations, 
+      running, 
+      post_calculations, 
+      generating_output, 
+      complete );
+
+   subtype Running_Phase is Phase_Type range run_starting .. generating_output;
+
+   type Health_Type is ( normal, aborted, in_error, queued );
+     
+end Model_Run_Settings_Types;
