48c175fea71154b45e81896ff53f7beb44f4a105
grahamstark <graham.stark@virtual-worlds.biz>
2015-11-10 21:25:45 +0000

    basic tasking support start, and some default data for test of state monitor

--- src/uk/model/model_run_settings_types.ads
+++ src/uk/model/model_run_settings_types.ads
@@ -7,14 +7,14 @@ package Model_Run_Settings_Types is
    type Target_Type is ( off_diagonal_index, costs_index, targetting_index );
   
    type Phase_Type is ( 
-      not_started, 
-      queued, 
-      run_starting, 
-      pre_calculations, 
-      running, 
-      post_calculations, 
-      generating_output, 
-      complete );
+      not_started, -- 0
+      queued,      -- 1
+      run_starting, -- 2
+      pre_calculations,  -- 3
+      running,  --4
+      post_calculations, -- 5 
+      generating_output, -- 6
+      complete );        -- 7
 
    subtype Running_Phase is Phase_Type range run_starting .. generating_output;
 
