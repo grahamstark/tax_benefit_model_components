dcaef272f156602f7dea12e587df9885c8366ca1
grahamstark <graham.stark@virtual-worlds.biz>
2016-06-21 11:34:27 +0100

    1st commit

--- src/uk/model/model-example_household.ads
+++ src/uk/model/model-example_household.ads
@@ -1,3 +1,23 @@
-with Base_Model_Types
+with Standard_UK_Enums;
 
-package Example_Household is
+package Model.Example_Household is
+   
+   type Person is record
+      pid : Sernum_Value;
+      age                       : Age_Range;
+      sex                       : Gender_Type;
+      incomes                   : Incomes_List;
+      employment                : Broad_Employment_Status;
+      is_contracted_in_to_serps : Boolean;
+      hours_worked              : Work_Hours_Range;
+    end record;  
+    
+    type Person_Array is array( Person_Number range <>  ) or Person;
+    
+    type Household( num_people : Person_Number := 1 ) is record
+       hid    : Sernum_Value;
+       tenure : Broad_Tenure_Type; 
+       people : Person_Array( 1 .. num_people );       
+    end record;
+                           
+end Model.Example_Household;
