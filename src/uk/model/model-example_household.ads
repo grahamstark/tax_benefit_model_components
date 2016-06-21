37190f734f7542b8dfdd020e3aab0dd3cd4fca68
grahamstark <graham.stark@virtual-worlds.biz>
2016-06-21 12:17:53 +0100

    more example stuff for abstract model - incomplete

--- src/uk/model/model-example_household.ads
+++ src/uk/model/model-example_household.ads
@@ -4,19 +4,22 @@ package Model.Example_Household is
    
    type Person is record
       pid : Sernum_Value;
-      age                       : Age_Range;
-      sex                       : Gender_Type;
-      incomes                   : Incomes_List;
-      employment                : Broad_Employment_Status;
-      is_contracted_in_to_serps : Boolean;
-      hours_worked              : Work_Hours_Range;
+      age                       : Age_Range := Age_Range'First;
+      sex                       : Gender_Type := male;
+      relationship              : Relationship_Type := single;
+      incomes                   : Incomes_List := ( others => 0.0 );
+      employment                : Broad_Employment_Status := full_time;
+      is_contracted_in_to_serps : Boolean := False; 
+      hours_worked              : Work_Hours_Range := Work_Hours_Range'First;
     end record;  
     
     type Person_Array is array( Person_Number range <>  ) or Person;
     
     type Household( num_people : Person_Number := 1 ) is record
-       hid    : Sernum_Value;
-       tenure : Broad_Tenure_Type; 
+       hid    : Sernum_Value := Sernum_Value'First;
+       year   : Year_Number := 2016;
+       tenure : Broad_Tenure_Type := social_rented; 
+       housing_costs : Housing_Array := ( others => 0.0 );
        people : Person_Array( 1 .. num_people );       
     end record;
                            
