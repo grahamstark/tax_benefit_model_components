dc15c5d6dc5ffb5d784884f10dc62e981141c106
Graham Stark <graham.stark@virtual-worlds.biz>
2016-06-22 15:04:46 +0100

    examples again

--- src/uk/model/model-example_household.ads
+++ src/uk/model/model-example_household.ads
@@ -6,7 +6,7 @@ package Model.Example_Household is
    use Standard_UK_Enums;
    use Ada.Calendar;
    
-   type Person is record
+   type Person is tagged record
       pid : Sernum_Value;
       age                       : Age_Range := Age_Range'First;
       sex                       : Gender_Type := male;
@@ -17,11 +17,16 @@ package Model.Example_Household is
       employment                : Broad_Employment_Status := full_time;
       is_contracted_in_to_serps : Boolean := False; 
       hours_worked              : Work_Hours_Range := Work_Hours_Range'First;
+      average_wage_per_hour     : Amount := 0.0;
     end record;  
     
     type Person_Array is array( Person_Number range <>  ) of Person;
     
-    type Household( num_people : Person_Number := 1 ) is record
+    type Benefit_Unit( num_people : Person_Number ) is tagged record
+       people : Person_Array( 1 .. num_people );
+    end record;
+    
+    type Household( num_people : Person_Number ) is tagged record
        hid           : Sernum_Value := Sernum_Value'First;
        year          : Year_Number := 2016;
        tenure        : Broad_Tenure_Type := social_rented; 
