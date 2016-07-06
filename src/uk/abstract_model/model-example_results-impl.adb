ffd69cab3d8d2149eb3ff290c958a9fafa8d40f4
Graham Stark <graham.stark@virtual-worlds.biz>
2016-07-06 21:31:59 +0100

    example implementation

--- src/uk/abstract_model/model-example_results-impl.adb
+++ src/uk/abstract_model/model-example_results-impl.adb
@@ -63,5 +63,62 @@ package body Model.Example_Results.Impl is
        result.income := ( others => 0.0 );
        result.intermediate := ( others => 0.0 );
     end Zero;
+    
+    
+    
+    procedure Set( 
+      result : in out Model_Benefit_Unit_Result;
+      pid    : Sernum_Value; 
+      value  : mar.Personal_Result'Class ) is
+    begin
+      for pno in result.people'Range loop
+         if result.people( pno ).pid = pid then
+            result.people( pno ) :=Personal_Result( value );
+         end if;         
+      end loop;
+    end Set;
+ 
+   function Get( 
+      result : Model_Benefit_Unit_Result; 
+      pid    : Sernum_Value ) return mar.Personal_Result'Class is
+      found : Boolean;
+      pres : Personal_Result := Personal_Result( result.Find( pid, found ));
+      mpers : Model_Personal_Result := ( pres with null record );
+   begin
+      return mpers;
+   end Get;
+      
+   procedure Set( 
+      result : in out Model_Benefit_Unit_Result;
+      pid    : Sernum_Value; 
+      which  : Calculated_Incomes_Range; 
+      value  : Amount;
+      op     : Operation_Type := Replace ) is
+      found : Boolean;
+   begin
+      for pno in result.people'Range loop
+         if pid = result.people( pno ).pid then
+            result.people( pno ).Set(
+               which,
+               value, 
+               op );            
+         end if;
+      end loop;
+   end Set;
+      
+   procedure Set( 
+      result : in out Model_Benefit_Unit_Result;
+      pid    : Sernum_Value; 
+      which  : Broad_Calculated_Type; 
+      value  : Amount;
+      op     : Operation_Type := Replace );
+      
+   function Get( 
+      result : Model_Benefit_Unit_Result; 
+      which  : Broad_Calculated_Type ) return Amount;
+      
+   procedure Zero( 
+      result : in out Model_Benefit_Unit_Result );
+
 
 end  Model.Example_Results.Impl;
--- src/uk/model/model-example_results.ads
+++ src/uk/model/model-example_results.ads
@@ -7,6 +7,7 @@ package Model.Example_Results is
    subtype Calculated_Incomes_List is T_Incomes.Abs_Amount_Array( Calculated_Incomes_Range'Range ); 
    
    type Personal_Result is tagged record
+      pid          : Sernum_Value;
       income       : Calculated_Incomes_List;
       intermediate : Broad_Calculated_List;
    end record;
@@ -16,10 +17,15 @@ package Model.Example_Results is
    type Household_Result( num_people : Person_Number ) is tagged record 
       people : Personal_Result_Array( 1 .. num_people );
    end record;
+
+   function Find( 
+      result : Household_Result; 
+      pid    : Sernum_Value; 
+      found  : out Boolean ) return Personal_Result'Class;
    
-   subtype Benefit_Unit_Result renames Household_Result;
+   subtype Benefit_Unit_Result is Household_Result;
    
    --we're assuming a single benefit uniut for now, so nothing else neeed here e.g. extract benefit unit from hhls 
-      
+    
 
 end Model.Example_Results;
