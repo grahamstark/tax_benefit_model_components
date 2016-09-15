53050dcfc756290ca650e79d1191b07b1e19499a
Graham Stark <graham.stark@virtual-worlds.biz>
2016-09-15 21:03:02 +0100

    summary cols missing

--- src/general/generic_hazards.ads
+++ src/general/generic_hazards.ads
@@ -1,12 +1,16 @@
 with Base_Model_Types;
+with Ada.Calendar;
 
 generic
    type Values_Range is (<>);
 package Generic_Hazards is
    
    use Base_Model_Types;
+   use Ada.Calendar;
    
    type Table_Type is array( Values_Range, Values_Range ) of Amount;
+   
+   type  Abs_Table_Type_Array is array( Year_Number range <> ) of Table_Type; 
    
    function To_Hazards( tab : in out Table_Type ) return Table_Type;
    
