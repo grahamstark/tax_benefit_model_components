72f44d4ddf6ef00a03e45c3ad0b1e51bcb702fb5
Graham Stark <graham.stark@virtual-worlds.biz>
2017-02-17 18:34:51 +0000

    simple BC generator port in C\#

---
+++ src/csharp/piecewise_linear_generator.cs
@@ -0,0 +1,254 @@
+//
+// copyright(c) 2017 Graham Stark/ Virtual Worlds (graham.stark@virtual-worlds.biz)
+//
+// ////////////////////////////////
+//
+// This is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 3, or (at your option)
+// any later version.
+// 
+// It is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this software; see the file docs/gpl_v3.  If not, write to
+// the Free Software Foundation, Inc., 51 Franklin Street,
+// Boston, MA 02110-1301, USA.
+// 
+//
+// Implementation of a Piecewise-Linear function generator. See Stark and Duncan 2001.
+//
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace PiecewiseLinearGenerator{
+
+        //
+        // just implemment this interface
+        //
+        public interface NetIncome{
+                
+                double GetNet( double gross );
+                
+        }
+        
+        // copied from: https://msdn.microsoft.com/en-us/library/87cdya3t(v=vs.110).aspx
+        public class GeneratorException: Exception{
+            public GeneratorException()
+            {
+            }
+        
+            public GeneratorException(string message)
+                : base(message)
+            {
+            }
+        
+            public GeneratorException(string message, Exception inner)
+                : base(message, inner)
+            {
+            }
+        }
+        
+        
+        // note there is a point class in Drawing .. 
+        public struct Point{
+                public double X {get;set;}
+                public double Y {get;set;}
+        }
+        
+        public struct Line{
+                public double A {get;set;}
+                public double B {get;set;}
+        }
+        
+        
+        
+        public class Generator{
+                
+                //
+                // fixme maybe make thes settable and nothing static .. 
+                //
+                const double VERTICAL  = 999999999.9999;
+                const double TOLERANCE = 0.001;
+                const double INCREMENT = 0.00001;
+                const int MAX_DEPTH    = 500;
+                
+                private static Line MakeLine( Point point_1, Point point_2 ){ 
+                        Line l;
+                        if( point_1.X == point_2.X ){
+                                 l.B = point_1.X;
+                                 l.A = VERTICAL;
+                                 return l;
+                        };
+                        l.B = (point_1.Y - point_2.Y)/(point_1.X - point_2.X );
+                        if( l.B < 0.0 ){
+                                l.B = l.B * (-1.0);
+                        };
+                        l.B = Math.Min( l.B, VERTICAL );
+                        if( l.B < 0.0 ){
+                                l.B = l.B * (-1.0);
+                        }
+                        l.A = ( point_1.Y - point_1.X*l.B );                
+                        return l;
+                }
+                
+                private static Point FindIntersection( Line line_1, Line line_2 ){
+                        Point p;
+                        if( line_1.B != line_2.B ){
+                                p.X = (line_2.A - line_1.A) / (line_1.B - line_2.B);
+                                p.Y = line_1.A + (p.X * line_1.B );
+                        } else {
+                                 p.X = 0.0;
+                                 p.Y = line_1.A;
+                        }
+                        return p;
+                }
+                
+                private static int ComparePoints( Point point_1, Point point_2 ){
+                        if( point_1.X > point_2.X ){
+                                return 1;       
+                        }
+                        if( point_1.X < point_2.X ){
+                                return -1;       
+                        }
+                        if( point_1.Y > point_2.Y ){
+                                return 1;               
+                        }
+                        if( point_1.Y < point_2.Y ){
+                                return -1;               
+                        }
+                        return 0;
+                }
+                
+                private static bool NearlySameLine( Line l1, Line l2 ){
+                        return ((( Math.Abs(l1.A-l2.A)) <= TOLERANCE ) && (( Math.Abs (l1.B-l2.B)) <= TOLERANCE ));
+                }
+        
+                private static bool NearlySamePoint( Point p1, Point p2 ){
+                        return ((( Math.Abs(p1.X-p2.X)) < TOLERANCE ) && (( Math.Abs (p1.Y-p2.Y)) < TOLERANCE));
+                }
+                
+                private static double Trunc( double x ){
+                        x *= 100.0;
+                        return ((double) Math.Truncate( x )/100.0);
+                }
+                
+                private static void Round( ref List<Point> points ){
+                        int n = points.Count;
+                        for( int i = 0; i < n; i++ ){
+                                Point p = points[i];
+                                p.X = Trunc( p.X );                        
+                                p.Y = Trunc( p.Y );
+                                points[i] = p;
+                        }
+                }
+                
+                private static void Censor( ref List<Point> points ){
+                        // Round( ref points );
+                        points.Sort( ComparePoints );
+                        int n = points.Count;
+                        if( n < 3 ){
+                                return;       
+                        }
+                        int i = 1;
+                        Line[] l = new Line[2];
+                        Point[] p = new Point[3];
+                        while( i < n-1 ){
+                                p[0] = points[i-1]; 
+                                p[1] = points[i]; 
+                                p[2] = points[i+1];
+                                l[0] = MakeLine( p[0], p[1] );
+                                l[1] = MakeLine( p[1], p[2] );
+                                if( NearlySameLine( l[0], l[1] )){
+                                        points.RemoveAt( i );
+                                        n = points.Count;
+                                } else {
+                                        i++;       
+                                }
+                        }
+                }
+                
+                public static void Generate(
+                        NetIncome calculator,
+                        ref List<Point> pointsList,
+                        ref int depth,
+                        double start_pos,
+                        double end_pos ){
+                        if( Math.Abs( start_pos - end_pos ) < TOLERANCE ){
+                                return;        
+                        }
+                        if( depth > MAX_DEPTH ){
+                                throw new GeneratorException( "Max Depth Exceeded" );                               
+                        }
+                        double anchor;
+                        Point[] points = new Point[5];
+                        Line[] lines = new Line[2];
+                        //
+                        // left line in points 1,2 
+                        //
+                        points[0].X = start_pos;
+                        points[0].Y = calculator.GetNet( points[0].X );
+                        points[1].X = start_pos + INCREMENT;
+                        points[1].Y = calculator.GetNet( points[1].X );
+                        //
+                        // right line in 3,4
+                        //
+                        points[3].X = end_pos;
+                        points[3].Y = calculator.GetNet( points[3].X );
+                        points[2].X = end_pos - INCREMENT;
+                        points[2].Y = calculator.GetNet( points[2].X );
+                        
+                        lines[0] = MakeLine( points[0], points[1] );
+                        lines[1] = MakeLine( points[2], points[3] );
+                        if( NearlySameLine( lines[0], lines[1] )){
+                                //
+                                // Same line, so the section we're testing represents a single straight segment.
+                                // We're done exploring this bit; store the line endpoints and return,
+                                //
+                                if( ! pointsList.Contains( points[0] )){
+                                        pointsList.Add( points[0] );
+                                }
+                                if( ! pointsList.Contains( points[3] )){
+                                        pointsList.Add( points[3] );
+                                }
+                                return;
+                        }
+                        //
+                        // If we're not done here, find where the lines meet and explore each side of there. 
+                        // If the meeting point is out range of the current section being tested, or exactly
+                        // at its edge, pick an arbitrary point between the line starts.
+                        //
+                        points[4] = FindIntersection( lines[0], lines[1] );
+                        if(( points[4].X <= start_pos ) || ( points[4].X >= end_pos )){
+                                anchor = start_pos + ( end_pos - start_pos ) / 2.0;
+                        } else {
+                                anchor = points[4].X;
+                        }
+                        depth += 1;
+                        //
+                        // recursively explore to the left of our new anchor point
+                        //
+                        Generate( calculator, ref pointsList, ref depth, start_pos, anchor );
+                        //
+                        // .. then to the right.
+                        //
+                        Generate( calculator, ref pointsList, ref depth, anchor, end_pos );
+                        depth -= 1;                
+                }
+        
+                
+                public static List<Point> Generate( NetIncome calculator ){
+                        List<Point> a = new List<Point>();
+                        int depth = 0;
+                        Generate( calculator, ref a, ref depth, 0.0, 10000.0 );
+                        Censor( ref a );
+                        return a;
+                }
+                
+        }
+}
---
+++ src/csharp/tests.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using PiecewiseLinearGenerator;
+
+//
+// simple test case: move this somewhere.. 
+//
+public class TBSys : NetIncome{
+       
+        private double it_allow = 500.0; 
+        
+        private double[] it_rate = new double[7]{ 0.1, 0.25, 0.4, 0.5, 0.7, 0.9, 1.0 };
+        private double[] it_band = new double[7]{ 2500, 4000, 5000, 8000, 9000, 10000, 9999999999999999999.99 };
+        
+        private double benefit = 150.0;
+        
+        // allowance and 4 tax rates
+        private double Tax( double gross ){
+                double ia = gross - it_allow;
+                if( ia < 0.0 ){
+                        return 0.0;       
+                }
+                double due = 0.0;
+                int i = 0;
+                double remaining = ia;
+                double last = 0.0;
+                while( remaining > 0.0 ){
+                        double gap = it_band[i] - last;
+                        double t = Math.Min( remaining, gap );
+                        due += t * it_rate[i];
+                        remaining -= gap;
+                        last = it_band[i];
+                        i++;
+                }
+                return due;
+        }
+        
+        // horizontal segment: give min 150 to everyone
+        private double Benefit( double gross ){
+                return ( gross < benefit ? benefit-gross : 0.0 );
+        }
+        
+        // vertical segment : give 30 to everyone earning over 200 euros
+        private double Benefit2( double gross ){
+                return ( gross > 200 ? 30.0 : 0.0 );
+        }
+        
+        public double GetNet( double gross ){
+                double tax = Tax( gross );
+                double benefit = Benefit( gross-tax ) + Benefit2( gross );
+                double net = gross - tax + benefit;
+                return net;       
+        }
+        
+}
+
+
+class Test{
+        
+        public static int Main( string[] args ){
+                TBSys sys = new TBSys();
+                List<Point> bc = Generator.Generate( sys );
+                for( int i = 0; i < bc.Count; i++ ){
+                        Console.WriteLine( "Point[{0}] : Gross = {1} Net = {2} ", i, bc[i].X, bc[i].Y );
+                }
+                return 0;
+        }
+
+}
+                
