72c24d875ae3181648b0273738dc74f9046b2d34
grahamstark <graham.stark@virtual-worlds.biz>
2018-04-02 16:22:50 +0100

    missing ads

---
+++ src/general/poverty_inequality_commons.ads
@@ -0,0 +1,32 @@
+package Poverty_Inequality_Commons is
+
+   type Measures is (
+      Mean,
+      Median, 
+      Average_Deviation,
+      Standard_Deviation,
+      Skewness,
+      Kurtosis,
+      Minimum,
+      Maximum,
+      Percentile_5,
+      Percentile_95,     
+      Total_Income, 
+      Total_Population,
+      Gini, 
+      MacRae_Inequality, 
+      Atkinson_Inequality, 
+      Kullback_Leibler_Inequality, 
+      Hoover_Inequality,
+      Europe_Equality, 
+      Europe_Inequality, 
+      Coulter_Inequality,
+      Platon_Inequality, 
+      Theil_Redundancy, 
+      Kullback_Leibler_Redundancy, 
+      Williamson_Variation,
+      Palma,
+      Negative_Warning ); 
+      
+      
+end  Poverty_Inequality_Commons;
